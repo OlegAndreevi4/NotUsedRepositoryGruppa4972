15469a1b23bc7e9508919ffebd57dcc9cfd88521
Gat <ivanzzzo@gmail.com>
2023-09-17 17:58:14 +0300

    Решено девять задач

--- Sem1Tack7/Program.cs
+++ Sem1Tack7/Program.cs
@@ -1,14 +1,18 @@
 ﻿// Напишите программу, которая на вход принимает трёхзначное число и на выходе показывает последнюю цифру этого числа.
 
+// Console.WriteLine("Введите число:");
+// int num = int.Parse(Console.ReadLine()??"0");
+// //if(num.ToString().Length = 3) Но всё со string - медленное
+// if(num > 100 && num < 1000)
+// {
+//     int lastDigit = num % 10;
+//     Console.WriteLine("Последняя цифра числа " + num + " это: " + lastDigit ); 
+// }
+// else
+// {
+//     Console.WriteLine("Число не трёхзначное");
+// } 
+
 Console.WriteLine("Введите число:");
 int num = int.Parse(Console.ReadLine()??"0");
-//if(num.ToString().Length = 3) Но всё со string - медленное
-if(num > 100 && num < 1000)
-{
-    int lastDigit = num % 10;
-    Console.WriteLine("Последняя цифра числа " + num + " это: " + lastDigit ); 
-}
-else
-{
-    Console.WriteLine("Число не трёхзначное");
-} 
+Console.WriteLine(num < 1000 && num > 99 ? num % 10: "Число не трёхзначное");
--- Sem1Tack8/Program.cs
+++ Sem1Tack8/Program.cs
@@ -1,4 +1,21 @@
 ﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-int numN = int.Parse((Console.ReadLine)??"0");
-while
+Console.WriteLine("Введите число:");
+int numN = int.Parse(Console.ReadLine()??"0");
+int i = 2;
+while(i < numN)
+{
+    Console.Write(i +", ");
+    i = i + 2;
+}
+Console.WriteLine(numN % 2 == 0 ? numN : "");
+
+// Console.WriteLine(numN % 2 == 0 ? "Число чётное" : "Число нечётное");
+
+// int evNumN = (-1) * numN;
+// while(evNumN < numN)
+// {
+//     Console.Write(evNumN + " ,");
+//     evNumN = evNumN + 1;
+// }
+// Console.WriteLine (numN);
